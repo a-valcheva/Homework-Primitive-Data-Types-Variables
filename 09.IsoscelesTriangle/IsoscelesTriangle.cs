d88183b88a91e5cb138ee4b8e5bc41a32db39b4b
anichka777 <avalcheva7@gmail.com>
2014-03-11 20:02:57 +0200

    UTF8

--- 09.IsoscelesTriangle/IsoscelesTriangle.cs
+++ 09.IsoscelesTriangle/IsoscelesTriangle.cs
@@ -13,6 +13,9 @@ class IsoscelesTriangle
     //symbol may still be displayed incorrectly, regardless of how much effort you put to fix it.
     static void Main(string[] args)
     {
+        //IMPORTANT Check your console font - if it's raster change it to Consolas with appropriate size or Lucida Console!
+        //If you don't want to do that put the next line in a comment.
+        Console.OutputEncoding = Encoding.UTF8;
         char copyRight = '\u00A9';
         Console.WriteLine("   {0}\n  {0} {0}\n {0}   {0}\n{0} {0} {0} {0}", copyRight);
     }
